9146e372b4f849036df47c6fb575e56065956041
alloutstudio <boovluna@gmail.com>
2025-07-16 04:02:47 +0800

    update

--- mailer.php
+++ mailer.php
@@ -4,7 +4,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "New Wholesale Inquiry";
 
     $name = htmlspecialchars($_POST["name"]);
-    $phone = htmlspecialchars($_POST["phone"]);
     $email = htmlspecialchars($_POST["email"]);
     $message = htmlspecialchars($_POST["message"]);
 
